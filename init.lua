b33916d66c4c5aee674d158ad9e0556da86be669
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-12-12 14:43:57 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,25 @@
+require("git"):setup()
+
+require("duckdb"):setup()
+
+-- Bookmarks
+require("bookmarks"):setup({
+	last_directory = {
+		enable = true,
+		persist = false,
+		mode = "dir",
+	},
+	persist = "vim",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
